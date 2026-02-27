46b4069ca1efea5cb3c7ef08d712cfa0ceb6058c
ahmed <ahmedmamdo38@gmail.com>
2026-02-27 20:31:47 +0200

    PowerPoint Tehtäviä

---
+++ Task2/Task2/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            OtsikkoLB = new Label();
+            TulostusLB = new Label();
+            ViestiTB = new TextBox();
+            TulostaBT = new Button();
+            SuspendLayout();
+            // 
+            // OtsikkoLB
+            // 
+            OtsikkoLB.AutoSize = true;
+            OtsikkoLB.Font = new Font("Segoe Script", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            OtsikkoLB.Location = new Point(22, 30);
+            OtsikkoLB.Name = "OtsikkoLB";
+            OtsikkoLB.Size = new Size(403, 51);
+            OtsikkoLB.TabIndex = 0;
+            OtsikkoLB.Text = "Anna tolustteva teksti:";
+            // 
+            // TulostusLB
+            // 
+            TulostusLB.AutoSize = true;
+            TulostusLB.Font = new Font("Segoe Script", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            TulostusLB.Location = new Point(22, 103);
+            TulostusLB.Name = "TulostusLB";
+            TulostusLB.Size = new Size(111, 51);
+            TulostusLB.TabIndex = 1;
+            TulostusLB.Text = "xxxx";
+            TulostusLB.Visible = false;
+            // 
+            // ViestiTB
+            // 
+            ViestiTB.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ViestiTB.Location = new Point(454, 42);
+            ViestiTB.Name = "ViestiTB";
+            ViestiTB.Size = new Size(405, 50);
+            ViestiTB.TabIndex = 2;
+            // 
+            // TulostaBT
+            // 
+            TulostaBT.ForeColor = SystemColors.MenuText;
+            TulostaBT.Location = new Point(698, 128);
+            TulostaBT.Name = "TulostaBT";
+            TulostaBT.Size = new Size(161, 49);
+            TulostaBT.TabIndex = 3;
+            TulostaBT.Text = "Tulosta teksti";
+            TulostaBT.UseVisualStyleBackColor = true;
+            TulostaBT.Click += TulostaBT_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Salmon;
+            ClientSize = new Size(931, 266);
+            Controls.Add(TulostaBT);
+            Controls.Add(ViestiTB);
+            Controls.Add(TulostusLB);
+            Controls.Add(OtsikkoLB);
+            ForeColor = Color.MintCream;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label OtsikkoLB;
+        private Label TulostusLB;
+        private TextBox ViestiTB;
+        private Button TulostaBT;
+    }
+}
